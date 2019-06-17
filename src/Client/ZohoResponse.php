3a9fc6ae2ca1cac80831657c0a2acef61a62e16a
Asadur Rahman <asadur.rahman@oscillosoft.com.au>
2019-06-17 12:46:19 +1000

    metadata content not found issue update

--- src/Client/ZohoResponse.php
+++ src/Client/ZohoResponse.php
@@ -233,6 +233,11 @@ class ZohoResponse
 		if ($this->http_status_code == 200) {
 			return $this->setSuccessResponse($json_response);
 		}
+		
+		if ($this->http_status_code == 204) {
+			$this->noContentException();
+		}
+		
 		$this->yieldException($json_response);
 	}
 
